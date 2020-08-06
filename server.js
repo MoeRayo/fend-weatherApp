4f9e26f8a140a480836863f0ef326d0b84f998bf
MoeRayo <moronfolu.motunrayo1@gmail.com>
2020-08-06 04:00:50 +0100

    feat:onfiguration for heroku

--- server.js
+++ server.js
@@ -22,8 +22,11 @@ app.use(express.static('website'));
 
 // Setup Server
 
-const port = 3000;
-const server = app.listen(port,listening);
+// const port = 3000;
+// const server = app.listen(port,listening);
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
 
 function listening(){
     console.log(`running on localhost: ${port}`);
